95c4347e6e5d24734f7bb32cf8739ada28bf09a1
Cassio Freitas <cassiofreitas@cassiofreitas.com>
2022-08-15 20:26:28 -0300

    Reorganizing MICROSOFTStuff

---
+++ MICROSOFTStuff/ResponsiveApp/ResponsiveApp/Helper/Converter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ResponsiveApp.Helper
+{
+    // isLessThanConverter //
+    public class IsLessThanConverter : IValueConverter
+    {
+        public static readonly IValueConverter Instance = new IsLessThanConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double doubleValue = System.Convert.ToDouble(value);
+            double compareToValue = System.Convert.ToDouble(parameter);
+
+            return doubleValue < compareToValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // IsGreateThanConverter //
+
+    public class IsGreaterThanConverter : IValueConverter
+    {
+        public static readonly IValueConverter Instance = new IsGreaterThanConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double doubleValue = System.Convert.ToDouble(value);
+            double compareToValue = System.Convert.ToDouble(parameter);
+
+            return doubleValue > compareToValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
