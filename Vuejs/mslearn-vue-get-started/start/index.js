de92ab2d7c3e8fde7cab365388f2307cba445df9
Cassio Freitas <cassiofreitas@cassiofreitas.com>
2022-08-20 17:54:20 -0300

    Using v-bind to variables and objects

--- Vuejs/mslearn-vue-get-started/start/index.js
+++ Vuejs/mslearn-vue-get-started/start/index.js
@@ -2,7 +2,12 @@ const app = Vue.createApp({
     data() {
         return {
             productName: 'Book a Cruise to the Moon',
-            productDescription: 'Cruise to the moon in our luxurious shuttle. '
+            productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+            productImage: './assets/cruise.jpg',
+            productImageDescription: 'An astronaut floats outside the window while you sit in comfort',
+            productImageStyle: {
+                'border-radius': '15px'
+            }
         }
     }
 });
