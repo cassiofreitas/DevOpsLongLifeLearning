0db6e69352178cf7c875be13a8c847049e65e45f
Cassio Freitas <cassiofreitas@cassiofreitas.com>
2022-08-20 17:07:08 -0300

    VueJS App instantiated and started

---
+++ Vuejs/mslearn-vue-get-started/start/index.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            productName: 'Book a Cruise to the Moon',
+            productDescription: 'Cruise to the moon in our luxurious shuttle. '
+        }
+    }
+});
+
+app.mount('#app');
