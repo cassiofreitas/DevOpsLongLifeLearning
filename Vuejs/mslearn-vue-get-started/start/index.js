64e13024e6bac306e16311ebe14badad9bf8a905
Cassio Freitas <cassiofreitas@cassiofreitas.com>
2022-08-20 21:20:02 -0300

    Using v-for with and without key; plus images

--- Vuejs/mslearn-vue-get-started/start/index.js
+++ Vuejs/mslearn-vue-get-started/start/index.js
@@ -7,7 +7,8 @@ const app = Vue.createApp({
             productImageDescription: 'An astronaut floats outside the window while you sit in comfort',
             productImageStyle: {
                 'border-radius': '15px'
-            }
+            },
+            itemsList: ['Susan', 'Peter', 'Bill', 'Mark', 'John']
         }
     }
 });
