bf577f0ff54f29a9593d8a8d41218f876dffa4e7
Cassio Freitas <cassiofreitas@cassiofreitas.com>
2022-09-15 16:25:46 -0300

    Excel Web Addin e Power Bi howto exercise

---
+++ Microsoft/ExcelWebAddIn1/ExcelWebAddIn1Web/Home.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+(function () {
+
+    Office.onReady(function () {
+        // Office is ready
+        $(document).ready(function () {
+            // The document is ready
+            $('#set-color').click(setColor);
+        });
+    });
+
+    async function setColor() {
+        await Excel.run(async (context) => {
+            const range = context.workbook.getSelectedRange();
+            range.format.fill.color = 'green';
+
+            await context.sync();
+        }).catch(function (error) {
+            console.log("Error: " + error);
+            if (error instanceof OfficeExtension.Error) {
+                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+            }
+        });
+    }
+})();
